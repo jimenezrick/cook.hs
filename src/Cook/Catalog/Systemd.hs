56bc83262836bff1024fc56a239dbe6580019f37
Ricardo Catalinas Jiménez <r@untroubled.be>
2016-03-29 11:31:03 +0100

    Improve Systemd module

--- src/Cook/Catalog/Systemd.hs
+++ src/Cook/Catalog/Systemd.hs
@@ -1,5 +1,7 @@
 module Cook.Catalog.Systemd (
     enableService
+  , disableService
+  , setHostname
   ) where
 
 import Text.Printf
@@ -9,3 +11,10 @@ import Cook.Recipe
 enableService :: String -> Recipe ()
 enableService name = withRecipeName "Systemd.EnableService" $ runProc "systemctl" ["enable", unit]
   where unit = printf "%s.service" name
+
+disableService :: String -> Recipe ()
+disableService name = withRecipeName "Systemd.DisableService" $ runProc "systemctl" ["disable", unit]
+  where unit = printf "%s.service" name
+
+setHostname :: String -> Recipe ()
+setHostname hostname = withRecipeName "Systemd.SetHostname" $ runProc "hostnamectl" ["set-hostname", hostname]
