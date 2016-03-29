3f362e25ba03af075c222cdc915146b5f4ed8cac
Ricardo Catalinas Jiménez <r@untroubled.be>
2016-03-30 00:45:43 +0100

    Add start/stop systemd service

--- src/Cook/Catalog/Systemd.hs
+++ src/Cook/Catalog/Systemd.hs
@@ -1,6 +1,8 @@
 module Cook.Catalog.Systemd (
     enableService
   , disableService
+  , startService
+  , stopService
   , setHostname
   ) where
 
@@ -14,6 +16,14 @@ enableService name = withRecipeName "Systemd.EnableService" $ runProc "systemctl
 
 disableService :: String -> Recipe ()
 disableService name = withRecipeName "Systemd.DisableService" $ runProc "systemctl" ["disable", unit]
+  where unit = printf "%s.service" name
+
+startService :: String -> Recipe ()
+startService name = withRecipeName "Systemd.StartService" $ runProc "systemctl" ["start", unit]
+  where unit = printf "%s.service" name
+
+stopService :: String -> Recipe ()
+stopService name = withRecipeName "Systemd.StopService" $ runProc "systemctl" ["stop", unit]
   where unit = printf "%s.service" name
 
 setHostname :: String -> Recipe ()
