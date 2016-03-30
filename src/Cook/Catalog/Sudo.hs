c3a8ae788066247b5a562ce6ce33d0c3f495f736
Ricardo Catalinas Jiménez <r@untroubled.be>
2016-03-30 10:56:46 +0100

    Keep SSH_AUTH_SOCK with sudo

--- src/Cook/Catalog/Sudo.hs
+++ src/Cook/Catalog/Sudo.hs
@@ -7,4 +7,5 @@ import Cook.Recipe
 enableSudoWheel :: Recipe ()
 enableSudoWheel = createFsTree "/etc/sudoers.d" tree
     where tree     = File "wheel-nopasswd" nopasswd (Just 0o440, Just ("root", "root"))
-          nopasswd = Content "%wheel ALL=(ALL) NOPASSWD: ALL"
+          nopasswd = Content "%wheel ALL=(ALL) NOPASSWD: ALL\n\
+                             \Defaults env_keep += \"SSH_AUTH_SOCK\"\n"
