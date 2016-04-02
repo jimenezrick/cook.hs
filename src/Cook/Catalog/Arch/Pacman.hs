494ee00e338f1253ecd0685a9c9b444736d5cd18
Ricardo Catalinas Jiménez <r@untroubled.be>
2016-04-02 22:06:59 +0100

    Arch/Pacman: upgrade packages if installation happens
    Also use non-empty list on type signatures.

--- src/Cook/Catalog/Arch/Pacman.hs
+++ src/Cook/Catalog/Arch/Pacman.hs
@@ -6,24 +6,32 @@ module Cook.Catalog.Arch.Pacman (
   ) where
 
 import Control.Monad
+import Data.List.NonEmpty
+import Data.Semigroup
 
 import Cook.Recipe
 
-pacman :: [String] -> Recipe ()
-pacman args = runProc "pacman" $ ["--quiet", "--noconfirm"] ++ args
+pacman :: NonEmpty String -> Step
+pacman args = proc "pacman" $ toList $ ["--quiet", "--noconfirm"] <> args
 
 upgradeInstalledPackages :: Recipe ()
-upgradeInstalledPackages = withRecipeName "Arch.Pacman.UpgradeInstalledPackages" $ pacman ["-Syu"]
+upgradeInstalledPackages = withRecipeName "Arch.Pacman.UpgradeInstalledPackages" $ do
+    run $ pacman ["-Syu"]
 
-installPackages :: [String] -> Recipe ()
-installPackages pkgs = withRecipeName "Arch.Pacman.InstallPackages" $ pacman $ ["--needed", "-S"] ++ pkgs
+installPackages :: NonEmpty String -> Recipe ()
+installPackages pkgs = withRecipeName "Arch.Pacman.InstallPackages" $ do
+    run $ pacman $ ["--needed", "-S"] <> pkgs
 
 isPackageInstalled :: String -> Recipe Bool
+isPackageInstalled ""  = error "isPackageInstalled: empty package name"
 isPackageInstalled pkg = withRecipeName "Arch.Pacman.IsPackageInstalled" $ do
-    err <- withoutError $ runRead $ proc "pacman" ["-Qq", pkg]
+    err <- withoutError $ runRead $ pacman ["-Q", pkg]
     either (const $ return False) (const $ return True) err
 
-requirePackages :: [String] -> Recipe ()
+requirePackages :: NonEmpty String -> Recipe ()
 requirePackages pkgs = withRecipeName "Arch.Pacman.RequirePackages" $ do
-    missingPkgs <- filterM isPackageInstalled pkgs
-    installPackages missingPkgs
+    missingPkgs <- filterM isPackageInstalled $ toList pkgs
+    case missingPkgs of
+        [] -> return ()
+        _  -> do upgradeInstalledPackages
+                 installPackages $ fromList missingPkgs
