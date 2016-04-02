e35338b313ff73eea856f09f7f9835d6208c410d
Ricardo Catalinas Jiménez <r@untroubled.be>
2016-04-02 19:30:59 +0100

    Add isPackageInstalled and requirePackages to Arch.Pacman module

--- src/Cook/Catalog/Arch/Pacman.hs
+++ src/Cook/Catalog/Arch/Pacman.hs
@@ -1,7 +1,11 @@
 module Cook.Catalog.Arch.Pacman (
     upgradeInstalledPackages
   , installPackages
+  , isPackageInstalled
+  , requirePackages
   ) where
+
+import Control.Monad
 
 import Cook.Recipe
 
@@ -9,7 +13,17 @@ pacman :: [String] -> Recipe ()
 pacman args = runProc "pacman" $ ["--quiet", "--noconfirm"] ++ args
 
 upgradeInstalledPackages :: Recipe ()
-upgradeInstalledPackages = withRecipeName "Pacman.UpgradeInstalledPackages" $ pacman ["-Syu"]
+upgradeInstalledPackages = withRecipeName "Arch.Pacman.UpgradeInstalledPackages" $ pacman ["-Syu"]
 
 installPackages :: [String] -> Recipe ()
-installPackages pkgs = withRecipeName "Pacman.InstallPackages" $ pacman $ ["--needed", "-S"] ++ pkgs
+installPackages pkgs = withRecipeName "Arch.Pacman.InstallPackages" $ pacman $ ["--needed", "-S"] ++ pkgs
+
+isPackageInstalled :: String -> Recipe Bool
+isPackageInstalled pkg = withRecipeName "Arch.Pacman.IsPackageInstalled" $ do
+    err <- withoutError $ runRead $ proc "pacman" ["-Qq", pkg]
+    either (const $ return False) (const $ return True) err
+
+requirePackages :: [String] -> Recipe ()
+requirePackages pkgs = withRecipeName "Arch.Pacman.RequirePackages" $ do
+    missingPkgs <- filterM isPackageInstalled pkgs
+    installPackages missingPkgs
