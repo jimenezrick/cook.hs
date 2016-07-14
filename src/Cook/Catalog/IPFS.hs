1ae6248d07899e10db505de9787d16be7e9ccd4e
Ricardo Catalinas Jiménez <r@untroubled.be>
2016-07-14 13:55:16 +0100

    Fix warnings

--- src/Cook/Catalog/IPFS.hs
+++ src/Cook/Catalog/IPFS.hs
@@ -2,6 +2,7 @@ module Cook.Catalog.IPFS (
     requireIPFS
   ) where
 
+import Control.Monad
 import Data.FileEmbed
 import Data.Text.Encoding
 
@@ -14,7 +15,7 @@ requireIPFS :: Recipe ()
 requireIPFS = withRecipeName "IPFS.RequireIPFS" $ do
     requirePackages ["go-ipfs"]
     addUser True [] "ipfs"
-    withSudoUser "ipfs" $ do
+    void $ withSudoUser "ipfs" $ do
         home <- getEnv "HOME"
         withCd home $ do
             withoutError $ runProc "ipfs" ["init"]
--- src/Cook/Recipe/Template.hs
+++ src/Cook/Recipe/Template.hs
@@ -29,37 +29,37 @@ type TemplateConf a = Tagged a FilePath
 asTemplate :: FilePath -> TemplateConf a
 asTemplate = Tagged
 
-useTemplate :: forall a. (Default a, Data a, Typeable a, Generic a, FromJSON a)
+useTemplate :: forall a. (Default a, Data a, Generic a, FromJSON a)
             => TemplateConf a -> FilePath -> IO ()
 useTemplate src dst = loadConf src >>= hastacheFile conf src' >>= Text.writeFile dst
   where conf = hastacheConf $ takeDirectory src'
         src' = untag src
 
-useTemplateNoDef :: forall a. (Data a, Typeable a, Generic a, FromJSON a)
+useTemplateNoDef :: forall a. (Data a, Generic a, FromJSON a)
                  => TemplateConf a -> FilePath -> IO ()
 useTemplateNoDef src dst = loadConfNoDef src >>= hastacheFile conf src' >>= Text.writeFile dst
   where conf = hastacheConf $ takeDirectory src'
         src' = untag src
 
-useTemplateWithDef :: forall a. (Data a, Typeable a, Generic a, FromJSON a)
+useTemplateWithDef :: forall a. (Data a, Generic a, FromJSON a)
                    => a -> TemplateConf a -> FilePath -> IO ()
 useTemplateWithDef withDef src dst = loadConfWithDef withDef src >>= hastacheFile conf src' >>= Text.writeFile dst
   where conf = hastacheConf $ takeDirectory src'
         src' = untag src
 
-loadConf :: forall a. (Default a, Data a, Typeable a, Generic a, FromJSON a)
+loadConf :: forall a. (Default a, Data a, Generic a, FromJSON a)
          => TemplateConf a -> IO (MuContext IO)
 loadConf = loadConf' $ mkGenericContext (def :: a)
 
-loadConfNoDef :: forall a. (Data a, Typeable a, Generic a, FromJSON a)
+loadConfNoDef :: forall a. (Data a, Generic a, FromJSON a)
               => TemplateConf a -> IO (MuContext IO)
 loadConfNoDef = loadConf' mempty
 
-loadConfWithDef :: forall a. (Data a, Typeable a, Generic a, FromJSON a)
+loadConfWithDef :: forall a. (Data a, Generic a, FromJSON a)
                 => a -> TemplateConf a -> IO (MuContext IO)
 loadConfWithDef withDef = loadConf' $ mkGenericContext withDef
 
-loadConf' :: forall a. (Data a, Typeable a, Generic a, FromJSON a)
+loadConf' :: forall a. (Data a, Generic a, FromJSON a)
           => MuContext IO -> TemplateConf a -> IO (MuContext IO)
 loadConf' withDef path = do
     vals <- decodeFileEither $ untag path -<.> "yaml"
