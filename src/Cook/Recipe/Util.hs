686c6fc8cbeb3d75f424b0e6e6d75ed11f6487ee
Ricardo Catalinas Jiménez <r@untroubled.be>
2016-09-21 17:44:17 +0100

    Add getHTTP

--- src/Cook/Recipe/Util.hs
+++ src/Cook/Recipe/Util.hs
@@ -1,8 +1,11 @@
 module Cook.Recipe.Util (
     withTempDir
+  , getHTTP
   ) where
 
+import Data.ByteString.Lazy (ByteString)
 import Data.Text.Lazy (unpack)
+import Network.HTTP.Simple
 
 import Cook.Recipe
 
@@ -13,3 +16,9 @@ withTempDir recipe = do
     a <- withCd tmpDir' recipe
     runProc "rm" ["-rf", tmpDir']
     return a
+
+getHTTP :: String -> Recipe ByteString
+getHTTP url = catchException $ do
+    req <- parseRequest url
+    res <- httpLBS req
+    return $ getResponseBody res
