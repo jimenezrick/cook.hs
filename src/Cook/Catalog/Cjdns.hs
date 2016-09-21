8d06e5508ea2c4e1103c996d325856210022c298
Ricardo Catalinas Jiménez <r@untroubled.be>
2016-09-21 10:02:59 +0100

    WIP

--- src/Cook/Catalog/Cjdns.hs
+++ src/Cook/Catalog/Cjdns.hs
@@ -4,20 +4,25 @@ module Cook.Catalog.Cjdns (
   ) where
 
 import Data.Aeson
+import Data.Aeson.Lens
 import Data.ByteString (ByteString)
-import Data.FileEmbed
 import Data.Text (Text)
 import GHC.Generics
+import System.FilePath.Find
 
 import Cook.Recipe
 import Cook.Recipe.Config
+import Cook.Recipe.Util
 import Cook.Catalog.Arch.Pacman
 import Cook.Catalog.Systemd
 
 --
--- TODO: Add peers, create passwords, nftables
+-- TODO: create passwords, nftables
+--
+-- Create ad-hoc recipe to generate random passwords
 --
 
+-- XXX: Validae this? not used any more
 data CjdnsOpts = CjdnsOpts
     { privateKey          :: Text
     , publicKey           :: Text
@@ -30,17 +35,51 @@ instance ToJSON CjdnsOpts
 
 instance FromJSON CjdnsOpts
 
-confTemplate :: ByteString
-confTemplate = $(embedFile "templates/cjdns/cjdroute.conf.mustache")
-
 requireCjdns :: FilePath -> Recipe ()
 requireCjdns confPath = withRecipeName "Cjdns.RequireCjdns" $ do
-    conf <- loadConfig confPath
+    conf <- loadConfig JSON confPath
     setUpCjdns conf
 
 setUpCjdns :: CjdnsOpts -> Recipe ()
 setUpCjdns opts = withRecipeName "SetUpCjdns" $ do
-    requirePackages ["cjdns"]
-    createFsTree "/etc" $ File "cjdroute.conf" (Template "cjdns" confTemplate opts) (Just 0o600, Just ("root", "root"))
-    enableService "cjdns"
-    startService "cjdns"
+    {-
+     -requirePackages ["cjdns", "curl"]
+     -createFsTree "/etc" $ File "cjdroute.conf" (Template "cjdns" confTemplate opts) (Just 0o600, Just ("root", "root"))
+     -enableService "cjdns"
+     -startService "cjdns"
+     -}
+
+    conf <- generateConfig
+    nodeConf <- loadConfig YAML "conf/cjdns/node.yaml"
+    peers <- getPeers
+
+    let conf'  = mergeConfig nodeConf conf
+        conf'' = insertConfigInto (key "interfaces" . key "UDPInterface" . nth 0 . key "connectTo") peers conf
+
+    conf''' <- writeConfig JSON conf''
+
+    createFsTree "/tmp" $ File "cjdroute.conf" (Content conf''') defAttrs
+
+getPeers :: Recipe Value
+getPeers = withRecipeName "GetPeers" $ do
+    peers <- withTempDir $ do
+        runSh "curl -s -L https://github.com/hyperboria/peers/archive/master.tar.gz | tar xz"
+        peerFiles <- liftIO $ find always (extension ==? ".k") "peers-master"
+
+        liftIO $ putStrLn "Loading peers:"
+        liftIO $ forM_ peerFiles putStrLn
+
+        peersList <- catMaybes <$> mapM (loadConfig JSON) peerFiles
+        writeConfig JSON $ foldl1 mergeConfig peersList
+
+    liftIO $ print peers
+
+    {-createFsTree "." $ File "peers.json" conf defAttrs-}
+    -- inject config
+
+
+
+generateConfig :: Recipe Value
+generateConfig = do
+    conf <- runPipeRead [proc "cjdroute" ["--genconf"], proc "cjdroute" ["--cleanconf"]]
+    return $ readConfig conf
