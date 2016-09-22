23f6e110bee6e69c60d4b97e252e4c1ce6d1b2ab
Ricardo Catalinas Jiménez <r@untroubled.be>
2016-09-22 16:26:04 +0100

    Add Cook.Catalog.Go

---
+++ src/Cook/Catalog/Go.hs
@@ -0,0 +1,16 @@
+module Cook.Catalog.Go (
+    goGet
+  ) where
+
+import System.FilePath ((</>))
+
+import Cook.Recipe
+import Cook.Recipe.Util
+
+goGet :: String -> Recipe ()
+goGet repo = withRecipeName "Go.goGet" $ do
+    requirePackages ["go"]
+    withTempDir $ \tmpDir -> do
+        withEnv [("GOPATH", tmpDir)] $ do
+            runProc "go" ["get", repo]
+            runProc "cp" [tmpDir </> "bin/*", "/usr/local/bin"]
