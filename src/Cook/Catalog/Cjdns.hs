f8ac231d8cf05aebc09ff16487d5391f396db36f
Ricardo Catalinas Jiménez <r@untroubled.be>
2016-09-25 23:51:30 +0100

    cjdns: Place IPv4 and IPv6 peers in the right section

--- src/Cook/Catalog/Cjdns.hs
+++ src/Cook/Catalog/Cjdns.hs
@@ -6,11 +6,15 @@ module Cook.Catalog.Cjdns (
 import Control.Monad
 import Control.Monad.IO.Class
 import Data.Aeson
+import Data.Aeson.Types
 import Data.Aeson.Lens
 import Data.Text.Lazy (Text)
 import Data.Maybe
 import GHC.Generics
 import System.FilePath.Find
+
+import qualified Data.HashMap.Strict as H
+import qualified Data.Text as T
 
 import Cook.Recipe
 import Cook.Recipe.Config
@@ -44,16 +48,18 @@ requireCjdns optsPath = withRecipeName "Cjdns.RequireCjdns" $ do
 setUpCjdns :: CjdnsOpts -> Recipe ()
 setUpCjdns opts = withRecipeName "SetUpCjdns" $ do
     defConf <- generateConfig
-    peers <- getPeers
+    (peersIPv4, peersIPv6) <- getPeers
     conf <- writeConfig JSON $
-            insertConfigInto (key "interfaces" . key "UDPInterface" . nth 0 . key "connectTo") peers $
+            insertConfigInto (key "interfaces" . key "UDPInterface" . nth 1 . key "connectTo") peersIPv6 $
+            insertConfigInto (key "interfaces" . key "UDPInterface" . nth 0 . key "connectTo") peersIPv4 $
             mergeConfig (toJSON opts) defConf
 
     createFsTree "/etc" $ File "cjdroute.conf" (Content conf) (Just 0o600, Just ("root", "root"))
     enableService "cjdns"
     startService "cjdns"
+    --createFsTree "." $ File "cjdroute.conf" (Content conf) defAttrs
 
-getPeers :: Recipe Value
+getPeers :: Recipe (Value, Value)
 getPeers = withRecipeName "GetPeers" $ do
     withTempDir $ \tmpDir -> do
         tarball <- getHTTP "https://github.com/hyperboria/peers/archive/master.tar.gz"
@@ -63,8 +69,11 @@ getPeers = withRecipeName "GetPeers" $ do
         liftIO $ putStrLn "Loading public peers:"
         liftIO $ forM_ peerFiles putStrLn
 
-        peersList <- catMaybes <$> mapM (loadConfig JSON) peerFiles
-        return $ foldl1 mergeConfig peersList
+        Object peers <- foldl mergeConfig emptyObject . catMaybes <$> mapM (loadConfig JSON) peerFiles
+        let peersIPv6 = H.filterWithKey isIPv6 peers
+            peersIPv4 = H.difference peers peersIPv6
+        return (Object peersIPv4, Object peersIPv6)
+  where isIPv6 a _ = T.head a == '['
 
 generateConfig :: Recipe Value
 generateConfig = withRecipeName "GenerateConfig" $ do
