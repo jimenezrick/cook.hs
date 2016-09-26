00d37835e85a2ff6c2c112b501ce9006c3e377e6
Ricardo Catalinas Jiménez <r@untroubled.be>
2016-09-26 18:34:50 +0100

    util: Add withFileContent

--- src/Cook/Recipe/Util.hs
+++ src/Cook/Recipe/Util.hs
@@ -1,16 +1,21 @@
 module Cook.Recipe.Util (
     withTempDir
   , getHTTP
+  , withFileContent
   ) where
 
+import Control.Monad.IO.Class
 import Data.ByteString.Lazy (ByteString)
+import Data.Text (Text)
 import Data.Text.Lazy (unpack)
 import Network.HTTP.Simple
+
+import qualified Data.Text.IO as T
 
 import Cook.Recipe
 
 withTempDir :: (FilePath -> Recipe a) -> Recipe a
-withTempDir recipe = withRecipeName "WithTempDir" $ do
+withTempDir recipe = withRecipeName "WithTempDir" $ catchException $ do
     (tmpDir, _) <- runRead $ proc "mktemp" ["--tmpdir", "--directory", "cook-XXXXXX"]
     let tmpDir' = head . lines $ unpack tmpDir
     a <- withCd tmpDir' $ recipe tmpDir'
@@ -22,3 +27,8 @@ getHTTP url = withRecipeName "GetHTTP" $ catchException $ do
     req <- parseRequest url
     res <- httpLBS req
     return $ getResponseBody res
+
+withFileContent :: FilePath -> (Text -> Text) -> Recipe ()
+withFileContent path f = withRecipeName "WithFileContent" $ catchException $ do
+    content <- liftIO $ T.readFile path
+    liftIO . T.writeFile path $ f content
