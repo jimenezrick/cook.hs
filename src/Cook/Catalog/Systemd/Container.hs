c36339ccb5081aa82cf9b469e8724e6d425c5498
Ricardo Catalinas Jiménez <r@untroubled.be>
2016-09-26 18:35:30 +0100

    Don't use withSudo unless it's essential

--- src/Cook/Catalog/Systemd/Container.hs
+++ src/Cook/Catalog/Systemd/Container.hs
@@ -9,7 +9,6 @@ import Cook.Recipe
 tarRootfs :: FilePath -> Recipe FilePath
 tarRootfs path = withRecipeName "Systemd.Container.TarRootfs" $ do
     let tarball = path <.> "tar.gz"
-    withSudo $ do
-        runProc "tar" ["cfz", tarball, "-C", path, "."]
-        runProc "rm" ["-r", path]
+    runProc "tar" ["cfz", tarball, "-C", path, "."]
+    runProc "rm" ["-r", path]
     return tarball
