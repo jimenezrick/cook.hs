123000be2a6fc0dd89607f780b1785b726c93d07
Ricardo Catalinas Jiménez <r@untroubled.be>
2016-09-26 18:35:02 +0100

    Add pts/0 to /etc/securetty

--- src/Cook/Catalog/Arch/Rootfs.hs
+++ src/Cook/Catalog/Arch/Rootfs.hs
@@ -4,12 +4,21 @@ module Cook.Catalog.Arch.Rootfs (
 
 import Control.Monad.IO.Class
 import System.Directory
+import System.FilePath
+
+import qualified Data.Text as T
 
 import Cook.Recipe
+import Cook.Recipe.Util
 
 buildRootfs :: FilePath -> Recipe FilePath
 buildRootfs path = withRecipeName "Arch.Rootfs.BuildRootfs" $ do
     liftIO $ createDirectory path
-    withSudo $ do
-        runProc "pacstrap" ["-i", "-c", "-d", path, "--noconfirm", "base"]
+    runProc "pacstrap" ["-i", "-c", "-d", path, "--noconfirm", "base"]
+    enablePts0 $ path </> "etc/securetty"
     return path
+
+enablePts0 :: FilePath -> Recipe ()
+enablePts0 securetty = withRecipeName "EnablePts0" $ do
+    withFileContent securetty $ T.unlines . insertPts0 . T.lines
+  where insertPts0 ls = init ls ++ ["pts/0", "", last ls]
