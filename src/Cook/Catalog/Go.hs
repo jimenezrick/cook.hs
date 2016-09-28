00ac74b174f23fcbb5a33ef4776e058c1b16a1a2
Ricardo Catalinas Jiménez <r@untroubled.be>
2016-09-28 09:45:17 +0100

    Fix Go

--- src/Cook/Catalog/Go.hs
+++ src/Cook/Catalog/Go.hs
@@ -2,7 +2,7 @@ module Cook.Catalog.Go (
     goGet
   ) where
 
-import System.FilePath ((</>))
+import System.FilePath ((</>), takeBaseName)
 
 import Cook.Recipe
 import Cook.Recipe.Util
@@ -10,8 +10,8 @@ import Cook.Catalog.Arch.Pacman
 
 goGet :: String -> Recipe ()
 goGet repo = withRecipeName "Go.Get" $ do
-    requirePackages ["go"]
+    requirePackages ["go", "git"]
     withTempDir $ \tmpDir -> do
         withEnv [("GOPATH", tmpDir)] $ do
             runProc "go" ["get", repo]
-            runProc "cp" [tmpDir </> "bin/*", "/usr/local/bin"]
+        runProc "cp" [tmpDir </> "bin" </> takeBaseName repo, "/usr/local/bin"]
