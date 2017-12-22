927fa135c12bfc81385acb0731497874b32357f4
Ricardo Catalinas Jiménez <r@untroubled.be>
2017-12-22 14:50:02 +0000

    Cook.Catalog.User: refactor to use long option names

--- src/Cook/Catalog/User.hs
+++ src/Cook/Catalog/User.hs
@@ -10,23 +10,23 @@ import Cook.Recipe
 
 addUser :: Bool -> [String] -> String -> Recipe f ()
 addUser createHome extraGroups user =
-    let homeFlag   = if createHome then ["-m"] else []
-        groupsFlag = if null extraGroups then [] else ["-G", intercalate "," extraGroups]
-    in withRecipeName "User.Add" $ do
+    let homeFlag = ["--create-home" | createHome]
+        groupsFlag = if null extraGroups then [] else ["--groups", intercalate "," extraGroups]
+    in withRecipeName "User.Add" $
         withoutErrorWhen userAlreadyExists $ runProc "useradd" $ homeFlag ++ groupsFlag ++ [user]
   where userAlreadyExists 9 = Just ()
         userAlreadyExists _ = Nothing
 
 delUser :: String -> Recipe f ()
-delUser user = withRecipeName "User.Delete" $ do
-    withoutErrorWhen userDontExists $ runProc "userdel" ["-r", user]
+delUser user = withRecipeName "User.Delete" $
+    withoutErrorWhen userDontExists $ runProc "userdel" ["--remove", user]
   where userDontExists 6 = Just ()
         userDontExists _ = Nothing
 
 addGroup :: Bool -> String -> Recipe f ()
 addGroup isSystem group =
-    let systemFlag   = if isSystem then ["--system"] else []
-    in withRecipeName "Group.Add" $ do
+    let systemFlag = ["--system" | isSystem]
+    in withRecipeName "Group.Add" $
         withoutErrorWhen groupAlreadyExists $ runProc "groupadd" $ systemFlag ++ [group]
   where groupAlreadyExists 9 = Just ()
         groupAlreadyExists _ = Nothing
