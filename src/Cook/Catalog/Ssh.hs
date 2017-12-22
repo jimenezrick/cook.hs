99f2b462369743a87260dbd946fddbbf1f063df6
Ricardo Catalinas Jiménez <r@untroubled.be>
2017-12-22 13:19:06 +0000

    Cook.Catalog.Ssh: Split authorizeKey{,File}

--- src/Cook/Catalog/Ssh.hs
+++ src/Cook/Catalog/Ssh.hs
@@ -1,11 +1,16 @@
-module Cook.Catalog.Ssh (
-    copyId
+module Cook.Catalog.Ssh
+  ( copyId
   , authorizeKey
+  , authorizeKeyFile
   , copyFile
   ) where
 
+import Data.ByteString.Lazy (ByteString)
 import System.FilePath
 import Text.Printf
+
+import qualified Data.ByteString.Lazy as B
+import qualified Data.Text.Lazy.Encoding as T
 
 import Cook.Recipe
 
@@ -13,14 +18,22 @@ copyId :: String -> String -> Recipe f ()
 copyId user host = withRecipeName "Ssh.CopyId" $ runProc "ssh-copy-id" [target]
   where target = printf "%s@%s" user host
 
-authorizeKey :: FilePath -> String -> Recipe f ()
-authorizeKey pubKeyPath user = withRecipeName "Ssh.AuthorizeKey" $ do
+authorizeKey :: ByteString -> String -> Recipe f ()
+authorizeKey pubKey user = withRecipeName "Ssh.AuthorizeKey" $
     createFsTree home tree
   where tree = Dir ".ssh" (Just 0o700, Just (user, user)) [
                    File "authorized_keys" key (Just 0o600, Just (user, user))
                ]
-        key  = Copy pubKeyPath
-        home = "/home" </> user
+        key  = Content $ T.decodeUtf8 pubKey
+        home = case user of
+                   "root" -> "/root"
+                   _      -> "/home" </> user
+
+authorizeKeyFile :: FilePath -> String -> Recipe f ()
+authorizeKeyFile path user = withRecipeName "Ssh.AuthorizeKeyFile" $ do
+    key <- recipeIO $ B.readFile path
+    authorizeKey key user
 
 copyFile :: String -> String -> FilePath -> FilePath -> Recipe f ()
-copyFile user host src dst = runProc "scp" [src, printf "%s@%s:%s" user host dst]
+copyFile user host src dst = withRecipeName "Ssh.CopyFile" $
+    runProc "scp" [src, printf "%s@%s:%s" user host dst]
